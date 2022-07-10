efe8fe57ad3903b7d6a4eb485e928577eb626a62
Fernando Mercadal <fermercadal@outlook.com>
2022-07-10 10:55:50 -0300

    Move data out

---
+++ components/data.tsx
@@ -0,0 +1,61 @@
+export const operations = [
+  {
+    value: "/",
+    id: "divide",
+  },
+  {
+    value: "*",
+    id: "multiply",
+  },
+  {
+    value: "-",
+    id: "subtract",
+  },
+  {
+    value: "+",
+    id: "add",
+  },
+];
+
+export const numbers = [
+  {
+    value: 7,
+    id: "seven",
+  },
+  {
+    value: 8,
+    id: "eight",
+  },
+  {
+    value: 9,
+    id: "nine",
+  },
+  {
+    value: 4,
+    id: "four",
+  },
+  {
+    value: 5,
+    id: "five",
+  },
+  {
+    value: 6,
+    id: "six",
+  },
+  {
+    value: 1,
+    id: "one",
+  },
+  {
+    value: 2,
+    id: "two",
+  },
+  {
+    value: 3,
+    id: "three",
+  },
+  {
+    value: 0,
+    id: "zero",
+  },
+];
